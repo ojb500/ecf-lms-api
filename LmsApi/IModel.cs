f4e35d2446523cf0c2aef6d8a4e02d54fce4f531
ojb500 <ojb@ojb500.co.uk>
2019-10-27 02:57:25 +0000

    fiddle

---
+++ LmsApi/IModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Ojb500.EcfLms
+{
+    public interface IModel
+    {
+        LeagueTable GetTable(string org, string name);
+        IEnumerable<Event> GetEvents(string org, string name);
+        IEnumerable<MatchCard> GetMatchCards(string org, string name);
+    }
+}
